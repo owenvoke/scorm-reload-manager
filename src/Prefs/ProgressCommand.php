74bdab2704d9f24919478b9b915892880a27f692
PXgamer <owzie123@gmail.com>
2017-08-24 17:21:47 +0100

    Added prefs:progress function

---
+++ src/Prefs/ProgressCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace pxgamer\ScormReload\Prefs;
+
+use pxgamer\ScormReload\Traits;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Class ProgressCommand
+ * @package pxgamer\ScormReload\Prefs
+ *
+ * @link http://www.reload.ac.uk/scormplayer.html - Reload SCORM Player homepage
+ */
+class ProgressCommand extends Command
+{
+    use Traits\Command;
+    use Traits\Preference;
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('prefs:progress')
+            ->setDescription('Enable or disable checking the auto-progression of items.')
+            ->addArgument('status', InputArgument::REQUIRED, 'A boolean variable.');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->oOutput = new SymfonyStyle($input, $output);
+
+        $this->setUser();
+        $this->setScormDirectory('SCORM_PLAYER_ROOT_DIR');
+
+        $bStatus = $input->getArgument('status');
+
+        $this->setPreferenceValue('auto_progress', $bStatus, 'Auto Progress');
+    }
+}
