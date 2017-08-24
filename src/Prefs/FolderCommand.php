4536164131a4e418e57a108d020b7861438d2de5
PXgamer <owzie123@gmail.com>
2017-08-24 17:11:57 +0100

    Added prefs:folder function

---
+++ src/Prefs/FolderCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace pxgamer\ScormReload\Prefs;
+
+use pxgamer\ScormReload\Traits;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Class FolderCommand
+ * @package pxgamer\ScormReload\Prefs
+ *
+ * @link http://www.reload.ac.uk/scormplayer.html - Reload SCORM Player homepage
+ */
+class FolderCommand extends Command
+{
+    use Traits\Command;
+    use Traits\Preference;
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('prefs:folder')
+            ->setDescription('Set the default folder directory.')
+            ->addArgument('directory', InputArgument::REQUIRED, 'Path to the new default directory.');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @return void
+     * @throws \ErrorException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->oOutput = new SymfonyStyle($input, $output);
+
+        $this->setUser();
+        $this->setScormDirectory('SCORM_PLAYER_ROOT_DIR');
+
+        $sDirectory = $input->getArgument('directory');
+
+        if (is_dir($sDirectory)) {
+            $this->setPreferenceValue('show_tree', $sDirectory, 'Default Directory');
+        } else {
+            throw new \ErrorException('Invalid directory: ' . $sDirectory);
+        }
+    }
+}
